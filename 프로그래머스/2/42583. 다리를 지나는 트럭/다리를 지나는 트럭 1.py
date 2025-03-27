6469c6aff04034c90c714bd998811f9aeb2468ea
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2025-03-27 19:02:45 +0900

    Create 다리를 지나는 트럭 1.py

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭 1.py
@@ -0,0 +1,18 @@
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    left_trucks = deque(truck_weights)
+    first = left_trucks.popleft()
+    on_bridge = deque([first])
+    on_bridge_time = deque([1])
+    time = 1
+    while on_bridge:
+        if time - on_bridge_time[0] == bridge_length - 1:
+            on_bridge.popleft()
+            on_bridge_time.popleft()
+        time += 1
+        if left_trucks:
+            if sum(on_bridge) + left_trucks[0] <= weight and len(on_bridge) + 1 <= bridge_length:
+                truck = left_trucks.popleft()
+                on_bridge.append(truck)
+                on_bridge_time.append(time)
+    return time
