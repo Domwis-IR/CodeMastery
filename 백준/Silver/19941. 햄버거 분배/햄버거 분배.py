ac7cdb60cdd3626521688477aea05de2c071cdc2
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2025-04-16 10:33:42 +0900

    [Silver III] Title: 햄버거 분배, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -2,19 +2,16 @@ def __main__():
     table_length, farthest_length = map(int, input().split())
     hamburger_line = list(input())
     max_people = 0
+
     for i, ch in enumerate(hamburger_line):
-        if ch == "H":
-            min_idx_person = len(hamburger_line)
-            can_eat = False
-            for j in range(farthest_length, -(farthest_length + 1), -1):
-                if 0 <= i + j < len(hamburger_line) and hamburger_line[i+j] == "P":
-                    min_idx_person = min(min_idx_person, i+j)
-                    can_eat = True
-            if can_eat:
-                max_people += 1
-                hamburger_line[i] = "X"
-                hamburger_line[min_idx_person] = "X"
-    
+        if ch == "P":
+            # 사람이면 왼쪽부터 가까운 햄버거를 찾음
+            for j in range(i - farthest_length, i + farthest_length + 1):
+                if 0 <= j < table_length and hamburger_line[j] == "H":
+                    hamburger_line[j] = "X"  # 햄버거 먹음
+                    max_people += 1
+                    break
+
     print(max_people)
 
 
